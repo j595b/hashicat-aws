d24900b6b8782be88b0161e635186803bfcad851
Jacob Aldridge <jacob.aldridge@erieinsurance.com>
2021-12-21 19:10:09 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
+    version = "2.2.0"
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket"
   acl    = "private"
-  bucket_prefix = "jacob aldridge"
 
   versioning = {
     enabled = true
